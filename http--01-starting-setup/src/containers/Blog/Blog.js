f8db2c2632593cc96a3193f9644ec2b091a1aa86
Juan Vasquez <kipvasq@gmail.com>
2019-02-09 11:40:11 -0600

    finished axios http tutorial

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -4,16 +4,18 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
-import axios from 'axios';
+// import axios from 'axios';
+import axios from '../../axios';
 
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('/posts')
             .then(response => {
                 const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
@@ -24,6 +26,9 @@ class Blog extends Component {
                 });
 
                 this.setState({ posts: updatedPosts });
+            })
+            .catch(error => {
+                this.setState({ error: true })
             });
     }
 
@@ -32,15 +37,18 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(
-            post => {
-                return <Post 
-                            key={post.id}
-                            title={post.title}
-                            author={post.author}
-                            clicked={() => this.postSelectedHandler(post.id)}
-                        />
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong...</p>
+        if(!this.state.error){
+            posts = this.state.posts.map(
+                post => {
+                    return <Post 
+                                key={post.id}
+                                title={post.title}
+                                author={post.author}
+                                clicked={() => this.postSelectedHandler(post.id)}
+                            />
             });
+        }
 
         return (
             <div>
--- http--01-starting-setup/src/index.js
+++ http--01-starting-setup/src/index.js
@@ -3,6 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+// default configuration used application-wide
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com/';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+var reqInt = axios.interceptors.request.use(request => {
+    console.log('[Request Interceptor]', request);
+    // edit the request config
+    return request;
+}, error => {
+    console.log('[Request Interceptor]', error);
+    return Promise.reject(error);
+});
+
+var resInt = axios.interceptors.response.use(response => {
+    console.log('[Response Interceptor]', response);
+    // edit the response config
+    return response;
+}, error => {
+    console.log('[Response Interceptor]', error);
+    return Promise.reject(error);
+});
+
+// eject interceptors
+axios.interceptors.request.eject(reqInt);
+axios.interceptors.request.eject(resInt);
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
